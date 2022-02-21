e8327daf0e88f7a3af7d5f80473ea72cc3cd5220
Andreas Lind-Johansen <>
2022-02-21 14:17:29 +0100

    Minor updates

--- source/60_aliases.sh
+++ source/60_aliases.sh
@@ -2,4 +2,6 @@ alias pri="cd ~/src/privat"
 alias dot="cd ~/.dotfiles"
 
 alias spk="cd ~/src/spk"
+alias pev="cd ~/src/spk/pensjonsvedtak"
 alias puma="cd ~/src/spk/puma-tjenestemoduler"
+alias pupev="cd ~/src/spk/puma-tjenestemoduler/pensjonsvedtak"
